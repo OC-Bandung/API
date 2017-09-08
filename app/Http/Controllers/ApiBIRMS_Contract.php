bda8f77008e6ad2e0e38e155af02bfe91fea607c
dc-coder <mir.mir@gmail.com>
2017-09-08 18:23:54 +0700

    many ways to build OC data

--- app/Http/Controllers/ApiBIRMS_Contract.php
+++ app/Http/Controllers/ApiBIRMS_Contract.php
@@ -30,63 +30,115 @@ LEFT JOIN 2016_birms_eproject_planning.tbl_pekerjaan ON tpekerjaan.pekerjaanID =
 LEFT JOIN 2016_birms_eproject_planning.tbl_sirup ON 2016_birms_eproject_planning.tbl_pekerjaan.sirupID = 2016_birms_eproject_planning.tbl_sirup.sirupID
 -- WHERE tpengadaan.ta = 2016 AND tbl_pekerjaan.sirupID <> 0 AND NOT ISNULL(tbl_pekerjaan.sirupID) 
 LIMIT 1';
+ 
+
+		$planning_sql = "SELECT pekerjaanID, 
+						2016_birms_eproject_planning.tbl_sirup.sirupID, 
+						namapekerjaan, tahun, nama 
+						FROM 
+						2016_birms_eproject_planning.tbl_sirup ,
+						2016_birms_eproject_planning.tbl_pekerjaan
+						where 2016_birms_eproject_planning.tbl_sirup.sirupID='3662192'
+						AND 2016_birms_eproject_planning.tbl_pekerjaan.sirupID = 2016_birms_eproject_planning.tbl_sirup.sirupID
+						";
+
+
+		$planning  = DB::select($planning_sql);	
+		$planning = $planning[0];
+
+
+		$pekerjaanID = $planning->pekerjaanID;
+
+
+		$tender_sql = "SELECT * FROM 2016_birms_econtract.tpekerjaan where pekerjaanID = '" . $pekerjaanID .  "'";	
+
+		$tender = DB::select($tender_sql);	
+		$tender = $tender[0];		
+
+		$pid = $tender->pid;
+
+
+		$items_sql = "SELECT * FROM 2016_birms_econtract.tpekerjaan where pid = '" . $pid .  "'";
+		$items = DB::select($items_sql);	
+
+		$tender_stage = array('pid' => $pid , 'items'=> $items );
+
+		$planning_tst = "http://localhost:8000/api/contracts/year/2016";
+
+
+		// $tender_slq = "SELECT * FROM 2016_birms_econtract.tpengadaan_pemenang";
 
 		
-		
-		$results = DB::select($sql);
-		$results = $results[0];
+		// $results = DB::select($sql);
+		// $results = $results[0];
     	
 
-    	$ocid = env('OCID') . $results->sirupID ;
+  //   	$ocid = env('OCID') . $results->sirupID ;
 
-		$id = '1';
-		$date = '20100101';
-		$tag = 'planning';
+		// $id = '1';
+		// $date = '20100101';
+		// $tag = 'planning';
 
-		$initiationType = 'tender';
+		// $initiationType = 'tender';
  
     	
  
- 		//////////////////////	
- 		// planning stage 	//
- 		//////////////////////
+ 	// 	//////////////////////	
+ 	// 	// planning stage 	//
+ 	// 	//////////////////////
 
     	
 
-    	$rationale = $results->nama;
-
-    	$project = array('project' =>   $results->namapekerjaan);
+  //   	$rationale = $results->nama;
+  //   	$project =  $results->namapekerjaan;
+  //   	$projectID=  $results->pekerjaanID;
+  //   	$source = $results->sumberdana;
  
-    	$planning_value = array('amount' =>  $results->anggaran, 'currency'=> env('CURRENCY') );
+  //   	$planning_value = array('amount' =>  $results->anggaran, 'currency'=> env('CURRENCY') );
 
-    	$budget = array(
-    					'id' => $results->kode ,
-    					 'description' => $results->sumberdana,
-    					 'value' => $planning_value
-    					);
-
-    	///compiling all stages together
-    	$planning_stage = array('rationale' =>  $rationale,
-    							'budget' => $budget );
-
-    	//////////////////////
-    	//////////////////////
-
-    	$tender_stage = array ("status" => "active");
+  //   	$budget = array(
+  //   					'id' => $results->kode ,
+  //   					 'description' => $results->sumberdana,
+  //   					 'value' => $planning_value,
+  //   					 'project' => $project,
+  //   					 'projectID' => $projectID,
+  //   					 'source' => $source
+  //   					);
 
 
-    	//return $results;
 
-    	$release  = array( 'ocid' => $ocid ,
-    						'id' => $id,
-    						'date' => $date,
-    						'tag' => $tag,
-    						'initiationType' => 'tender',
-    						'planning' => $planning_stage,
-    						'tender' => $tender_stage
+  //   	///compiling all stages together
+  //   	$planning_stage = array('rationale' =>  $rationale,
+  //   							'budget' => $budget );
+
+		// //////////////////////	
+ 	// 	// tender stage 	//
+ 	// 	//////////////////////
+
+  //   	$tender_id = $results->pekerjaanID;
+  //   	$tender_title = $results->namapekerjaan;
+  //   	$tender_status= $results->stat;
+  //   	$tender_procurementMethod = $results->metode_pengadaan;
+
+  //   	$tender_stage = array ("id" => $tender_id,
+  //   							"tender_title" => $tender_title,
+  //   							"tender_status" => $tender_status,
+  //   							"procurementMethod" => $tender_procurementMethod
+  //   							);
+
+
+  //   	//return $results;
+
+  //   	$release  = array( 'ocid' => $ocid ,
+  //   						'id' => $id,
+  //   						'date' => $date,
+  //   						'tag' => $tag,
+  //   						'initiationType' => 'tender',
+  //   						'planning' => $planning_stage,
+  //   						'tender' => $tender_stage
     						
-    					  );
-    	return response()->json($release)->header('Access-Control-Allow-Origin', '*');
+  //   					  );
+    	return response()->json($planning_tst)->header('Access-Control-Allow-Origin', '*');
 
     	  // return response()->json($results)->header('Access-Control-Allow-Origin', '*');
 	  
